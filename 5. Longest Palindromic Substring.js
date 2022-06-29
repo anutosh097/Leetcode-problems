11227c79d4a54b027ae65ccaf07c15386f1b9c8c
anutosh097 <anutosh053@gmail.com>
2022-06-29 23:04:16 +0530

    5. Longest Palindromic Substring

--- 5. Longest Palindromic Substring.js
+++ 5. Longest Palindromic Substring.js
@@ -9,14 +9,14 @@ Time Complexity: - O(N*N)
 SPace Complexity:- O(1)
 */
 
-var longestPalindrome = function(s) {
+var longestPalindrome = function (s) {
     let startIndex = 0;
-    let maxLength =1;
+    let maxLength = 1;
     //helper function that expands around the center and keeps track of startIndex and currentPalLength
-    function expandAroundMiddle(left, right){
-        while(left >= 0 && right < s.length && s[left] === s[right]){
+    function expandAroundMiddle(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
             let currentPalLength = right - left + 1;
-            if(currentPalLength > maxLength){
+            if (currentPalLength > maxLength) {
                 maxLength = currentPalLength
                 startIndex = left
             }
@@ -24,10 +24,10 @@ var longestPalindrome = function(s) {
             right += 1
         }
     }
-    
-    for(let i =0; i < s.length; i++){
-        expandAroundMiddle(i+1,i-1); //for odd number strings
-        expandAroundMiddle(i,i+1) //for even number strings
+
+    for (let i = 0; i < s.length; i++) {
+        expandAroundMiddle(i + 1, i - 1); //for odd number strings
+        expandAroundMiddle(i, i + 1) //for even number strings
     }
     //return substring
     return s.slice(startIndex, startIndex + maxLength)
