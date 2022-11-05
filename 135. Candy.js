72226ef11eeda2db512e052fda31e26ca9d44eee
anutosh097 <anutosh053@gmail.com>
2022-11-05 09:32:59 +0530

    all recent problems added

--- 135. Candy.js
+++ 135. Candy.js
@@ -3,28 +3,34 @@ https://leetcode.com/problems/candy/
 */
 
 
-var candy = function(ratings) {
-    let len = ratings.length;
-let forward = new Array(len);
-let backward = new Array(len);
-/*
-take a forward and backward array of same length as ratings and then
-fill the arrays according to below logic, at the end loop through forward and backward and take max for each position and add to sum, return sum which is answer
-*/
-forward[0] = 1;
-backward[len-1] = 1;
-for (let i = 1; i < len; i++) {
-    if (ratings[i] > ratings[i-1]) forward[i] = forward[i-1] + 1;
-    else forward[i] = 1;
-}
-
-for (let i = len - 2; i >= 0; i--) {
-    if (ratings[i] > ratings[i+1]) backward[i] = backward[i+1] + 1;
-    else backward[i] = 1;
-}
-let sum = 0;
-for (let i = 0; i < len; i++) {
-    sum += Math.max(forward[i], backward[i]);
-}
-return sum;
-};
+/**
+ * @param {number[]} ratings
+ * @return {number}
+ */
+ var candy = function(ratings) {
+    let len = ratings.length
+    let candies = new Array(len).fill(1)
+     /*
+     take a forward and backward array of same length as ratings and then
+     fill the arrays according to below logic, at the end loop through forward and backward and take max for each position and add to sum, return sum which is answer
+     */
+  
+      
+      //left to right traversal and considering left neighbour of every ith rating
+      for (let i = 1; i < len; i++) {
+          if (ratings[i] > ratings[i-1]) candies[i] = candies[i-1] + 1
+      }
+  
+     //right to left traversal and considering right neighbour of every ith rating
+      for (let i = len - 2; i >= 0; i--) {
+          if (ratings[i] > ratings[i+1]) candies[i] = Math.max(candies[i], candies[i+1]+1)
+      }
+      console.log(candies)
+      
+      ///get sum of candies
+      let sum = 0
+      for (let i = 0; i < len; i++) {
+          sum += candies[i]
+      }
+      return sum
+  };
