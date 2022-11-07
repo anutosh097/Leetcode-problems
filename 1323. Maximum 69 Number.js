9620bcb0f16e491dbd9b4985b9bf4bf510faa5ce
anutosh097 <anutosh053@gmail.com>
2022-11-08 00:59:47 +0530

    1323. Maximum 69 Number

---
+++ 1323. Maximum 69 Number.js
@@ -0,0 +1,11 @@
+/*
+https://leetcode.com/problems/maximum-69-number/
+*/
+
+/**
+ * @param {number} num
+ * @return {number}
+ */
+ var maximum69Number  = function(num) {
+    return Number(num.toString().replace('6', '9'))
+    }    
